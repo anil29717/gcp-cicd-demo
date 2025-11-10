589e74c25f92ba01beba12813d0e277e4d50a562
Anil Kumar <93667898+anil29717@users.noreply.github.com>
2025-11-10 14:19:55 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
